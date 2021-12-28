8d379fdeb695f6f954b4531224f10c432be40cdd
黄中威 <283401795@qq.com>
2021-12-28 22:54:26 +0800

    index分支

---
+++ src/components/Header/HeaderTop.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="headertop">
+    <div class="container">
+      <div class="header_img">
+        <img class="header_img" src="../../assets/images/关 闭@2x.png" alt="" />
+      </div>
+      <span class="header_title">客户小助手</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "headertop",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+#headertop {
+  .container {
+    position: relative;
+    text-align: center;
+    background-color: #4477bc;
+    color: #fff;
+    height: 0.8rem;
+    line-height: 0.8rem;
+    .header_img {
+      position: absolute;
+      display: inline-block;
+      left: 0;
+      height: 0.32rem;
+      width: 0.32rem;
+    }
+    .header_title {
+      font-size: 0.3rem;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="home">home page</div>
+  <div id="home">
+    <div class="container">
+      <div>
+        <HeaderTop></HeaderTop>
+      </div>
+      <div>下</div>
+      <div>内容</div>
+    </div>
+  </div>
 </template>
 
 <script>
+const HeaderTop = () =>
+  import(/* webpackChunkName: "headertop" */ "../components/Header/HeaderTop");
 export default {
   name: "Home",
-  components: {},
+  components: {
+    HeaderTop,
+  },
 };
 </script>
 <style lang="scss" scoped>
+#home {
+  .container {
+    padding: 0 0.25rem;
+    background-color: #4477bc;
+  }
+}
 </style>
